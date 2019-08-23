564288bdd1cca5fdfa363b1d3c9833a5f8066ec5
袁大伟 <yuandawei@colipu.com>
2019-08-23 20:05:06 +0800

    add files

---
+++ UnitOfWorkLibary/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitOfWorkLibary.Entites;
+
+namespace UnitOfWorkLibary
+{
+    public class MyDbContext : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }
+
+        public virtual DbSet<Techer> Techers { get; set; }
+
+        public MyDbContext(string connectionString)
+        {
+
+        }
+    }
+}
---
+++ UnitOfWorkLibary/Resposities/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitOfWorkLibary.Resposities
+{
+    public interface IRepository<T> where T:class
+    {
+        IQueryable<T> Entites { get; }
+
+        void Remove(T entity);
+
+        void Add(T entity);
+    }
+
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        public IQueryable<T> Entites => throw new NotImplementedException();
+
+        public BaseRepository()
+        {
+
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
